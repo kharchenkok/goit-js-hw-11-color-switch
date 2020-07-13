ee4c3f1643ae57a8bcbd12eab9d88dc59e98a4df
kharchenkok <k.v.kharchenko@ukr.net>
2020-07-13 22:02:32 +0300

    hw-11-color-switch-second-added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const refs={
     stopButton:document.querySelector('[data-action="stop"]'),
     body:document.querySelector('body')
 }
-// console.log(refs.startButton);
+
 const colors = [
     '#FFFFFF',
     '#2196F3',
@@ -17,21 +17,54 @@ const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-let timerId=null
+  // ==============================1ex===========================================================
+// let timerId=null
 
-const startSwitchColor = () => {
-    timerId = setInterval( () => {
-            const currentColor = colors[randomIntegerFromInterval(0,colors.length-1)];
-            refs.body.style.backgroundColor=currentColor;
-            // console.log(currentColor);
-    }, 1000);
-    refs.startButton.removeEventListener('click', startSwitchColor)
+// const startSwitchColor = () => {
+//     timerId = setInterval( () => {
+//             const currentColor = colors[randomIntegerFromInterval(0,colors.length-1)];
+//             refs.body.style.backgroundColor=currentColor;
+//             // console.log(currentColor);
+//     }, 1000);
+//     refs.startButton.removeEventListener('click', startSwitchColor)
     
-};
-const stopSwitchColor = () =>{
-    clearInterval(timerId);
-    refs.startButton.addEventListener('click', startSwitchColor);
+// };
+// const stopSwitchColor = () =>{
+//     clearInterval(timerId);
+//     refs.startButton.addEventListener('click', startSwitchColor);
+// }
+
+//   refs.startButton.addEventListener('click', startSwitchColor);
+//   refs.stopButton.addEventListener('click', stopSwitchColor);
+
+// ==============================2ex========================================================
+const switchBgColor = {
+  timerId:null,
+  isActive:false,
+
+  start () {
+    if(this.isActive){
+      return
+    }
+    this.isActive=true;
+    this.timerId = setInterval(()=>{
+        const currentColor = colors[randomIntegerFromInterval(0,colors.length-1)];
+        switchColor(currentColor)
+    },1000 )
+  },
+
+  stop(){
+    clearInterval(this.timerId),
+    this.timerId=null
+    this.isActive=false
+    switchColor(colors[0])
+  }
 }
 
-  refs.startButton.addEventListener('click', startSwitchColor);
-  refs.stopButton.addEventListener('click', stopSwitchColor);
+function switchColor(color){
+  refs.body.style.backgroundColor=color;
+}
+
+refs.startButton.addEventListener('click', switchBgColor.start.bind(switchBgColor));
+
+refs.stopButton.addEventListener('click', switchBgColor.stop.bind(switchBgColor));
